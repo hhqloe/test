a638bb39840f22c92bf80caca7f35cdd03b215cc
hhqloe <1281161332@qq.com>
2026-01-28 16:45:41 +0800

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__ = 'Huiqiang Huang'
+import sys
+import os
+
+script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+print(script_dir)
+
+
